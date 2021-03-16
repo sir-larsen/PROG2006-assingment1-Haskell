f255f8caa76d464f3ae76f5c6d37ac589234a8c5
Alexander <alexael@ntnu.no>
2021-03-16 02:40:51 +0100

    Implemented command-line arguments for starting different modes, code also refactored and separated

---
+++ src/Iofuncs.hs
@@ -0,0 +1,290 @@
+module Iofuncs where
+
+import Lib
+import System.Random
+
+-- | Function for printing winning message
+winMessage :: Symbol -> IO ()
+winMessage sym = do
+    let line = "GAME OVER " ++ show sym ++ " WON"
+    putStrLn line
+
+-- | Human machine game loop
+gameLoopHM2 :: Bool -> Symbol -> [Cell] -> IO ()
+gameLoopHM2 play sym board = do
+    if play == True then do
+        renderBoard board
+        cell <- getLine
+
+        let command = words cell
+
+        let index = getBoardIndex2 (command !! 0)
+        let check = checkIsFree2 board index
+        if check == True then do
+            if checkRot command == True then do
+                if checkRotRight (command !! 1) == True then do
+                    let newBoard = assignCell (command !! 0) sym board
+                    let newBoard2 = rotSwapRight newBoard
+                    
+                    --Check winner
+                    let win = checkWin sym newBoard2
+                    if win == True then do
+                        renderBoard newBoard2
+                        winMessage sym
+                        return ()
+                    else do
+                        --Check draw
+                        if length (possibleMoves4 newBoard2) == 0 then do
+                            renderBoard newBoard2
+                            putStrLn "DRAW: GAME OVER"
+                            return ()
+                        else do
+                            gameLoopHM2 False O newBoard2
+                else do
+                    let newBoard = assignCell (command !! 0) sym board
+                    let newBoard2 = rotSwapLeft newBoard
+                    --Check winner
+                    let win = checkWin sym newBoard2
+                    if win == True then do
+                        renderBoard newBoard2
+                        winMessage sym
+                        return ()
+                    else do
+                        --Check draw
+                        if length (possibleMoves4 newBoard2) == 0 then do
+                            renderBoard newBoard2
+                            putStrLn "DRAW: GAME OVER"
+                            return ()
+                        else do
+                            gameLoopHM2 False O newBoard2
+            else do
+                let newBoard = assignCell (command !! 0) sym board
+                
+                let win = checkWin sym newBoard
+                if win == True then do
+                    renderBoard newBoard
+                    winMessage sym
+                    return ()
+                else do
+                    if length (possibleMoves4 newBoard) == 0 then do
+                        renderBoard newBoard
+                        putStrLn "DRAW: GAME OVER"
+                        return ()
+                    else do
+                        gameLoopHM2 False O newBoard
+        else do
+            putStrLn "GAME OVER X LOST"
+            return ()
+    else do
+        --let loop = do
+        num <- getStdRandom $ randomR (0, 8 :: Int)
+        let moves = possibleMoves4 board
+        let x = elem num moves
+
+        if x == True then do
+            let newBoard = assignCell2 num sym board
+            --Check win and draw
+            let win = checkWin sym newBoard
+            if win == True then do
+                renderBoard newBoard
+                winMessage sym
+                return ()
+            else do
+                if length (possibleMoves4 newBoard) == 0 then do
+                    renderBoard newBoard
+                    putStrLn "DRAW: GAME OVER"
+                    return ()
+                else do
+                    gameLoopHM2 True X newBoard
+        else do gameLoopHM2 False O board
+
+-- | Human vs human game loop
+gameLoopHH :: Bool -> Symbol -> [Cell] -> IO ()
+gameLoopHH play sym board = do
+    if play == True then do
+        renderBoard board
+        cell <- getLine
+
+        let command = words cell
+    
+        let index = getBoardIndex2 (command !! 0)
+        let check = checkIsFree2 board index
+        if check == True then do
+            if checkRot command == True then do
+                if checkRotRight (command !! 1) == True then do
+                    let newBoard = assignCell (command !! 0) sym board
+                    let newBoard2 = rotSwapRight newBoard
+                    
+                    --Check winner
+                    let win = checkWin sym newBoard2
+                    let win2 = checkWin O newBoard2
+                    if win == True || win2 == True then do
+                        renderBoard newBoard2
+                        winMessage sym
+                        return ()
+                    else do
+                        --Check draw
+                        if length (possibleMoves4 newBoard2) == 0 then do
+                            renderBoard newBoard2
+                            putStrLn "DRAW: GAME OVER"
+                            return ()
+                        else do
+                            gameLoopHH False O newBoard2
+                else do
+                    let newBoard = assignCell (command !! 0) sym board
+                    let newBoard2 = rotSwapLeft newBoard
+                    --Check winner
+                    let win = checkWin sym newBoard2
+                    let win2 = checkWin O newBoard2
+                    if win == True || win2 == True then do
+                        renderBoard newBoard2
+                        winMessage sym
+                        return ()
+                    else do
+                        --Check draw
+                        if length (possibleMoves4 newBoard2) == 0 then do
+                            renderBoard newBoard2
+                            putStrLn "DRAW: GAME OVER"
+                            return ()
+                        else do
+                            gameLoopHH False O newBoard2
+            else do
+                let newBoard = assignCell (command !! 0) sym board
+                
+                let win = checkWin sym newBoard
+                let win2 = checkWin O newBoard
+                if win == True || win2 == True then do
+                    renderBoard newBoard
+                    winMessage sym
+                    return ()
+                else do
+                    if length (possibleMoves4 newBoard) == 0 then do
+                        renderBoard newBoard
+                        putStrLn "DRAW: GAME OVER"
+                        return ()
+                    else do
+                        gameLoopHH False O newBoard
+        else do
+            renderBoard board
+            putStrLn "GAME OVER X LOST"
+            return ()
+    else do
+        renderBoard board
+        cell <- getLine
+
+        let command = words cell
+
+        let index = getBoardIndex2 (command !! 0)
+        let check = checkIsFree2 board index
+        if check == True then do
+            if checkRot command == True then do
+                if checkRotRight (command !! 1) == True then do
+                    let newBoard = assignCell (command !! 0) sym board
+                    let newBoard2 = rotSwapRight newBoard
+                    
+                    --Check winner
+                    let win = checkWin sym newBoard2
+                    let win2 = checkWin X newBoard2
+                    if win == True || win2 == True then do
+                        renderBoard newBoard2
+                        winMessage sym
+                        return ()
+                    else do
+                        --Check draw
+                        if length (possibleMoves4 newBoard2) == 0 then do
+                            renderBoard newBoard2
+                            putStrLn "DRAW: GAME OVER"
+                            return ()
+                        else do
+                            gameLoopHH True X newBoard2
+                else do
+                    let newBoard = assignCell (command !! 0) sym board
+                    let newBoard2 = rotSwapLeft newBoard
+                    --Check winner
+                    let win = checkWin sym newBoard2
+                    let win2 = checkWin X newBoard2
+                    if win == True || win2 == True then do
+                        renderBoard newBoard2
+                        winMessage sym
+                        return ()
+                    else do
+                        --Check draw
+                        if length (possibleMoves4 newBoard2) == 0 then do
+                            renderBoard newBoard2
+                            putStrLn "DRAW: GAME OVER"
+                            return ()
+                        else do
+                            gameLoopHH True X newBoard2
+            else do
+                let newBoard = assignCell (command !! 0) sym board
+                
+                let win = checkWin sym newBoard
+                let win2 = checkWin X newBoard
+                if win == True || win2 == True then do
+                    renderBoard newBoard
+                    winMessage sym
+                    return ()
+                else do
+                    if length (possibleMoves4 newBoard) == 0 then do
+                        renderBoard newBoard
+                        putStrLn "DRAW: GAME OVER"
+                        return ()
+                    else do
+                        gameLoopHH True X newBoard
+        else do
+            renderBoard board
+            putStrLn "GAME OVER O LOST"
+            return ()
+
+-- | Machine vs machine game loop
+gameLoopMM :: Bool -> Symbol -> [Cell] -> IO ()
+gameLoopMM play sym board = do
+    if play == True then do
+        num <- getStdRandom $ randomR (0, 8 :: Int) -- Get random move
+        let moves = possibleMoves4 board
+        let x = elem num moves
+
+        if x == True then do
+            let newBoard = assignCell2 num sym board
+            renderBoard board
+            putStrLn ""
+            --Check win and draw
+            let win = checkWin sym newBoard
+            if win == True then do
+                renderBoard newBoard
+                winMessage sym
+                return ()
+            else do
+                if length (possibleMoves4 newBoard) == 0 then do
+                    renderBoard newBoard
+                    putStrLn "DRAW: GAME OVER"
+                    return ()
+                else do
+                    gameLoopMM False O newBoard
+        else do
+            gameLoopMM True X board
+    else do
+        num <- getStdRandom $ randomR (0, 8 :: Int)
+        let moves = possibleMoves4 board
+        let x = elem num moves
+
+        if x == True then do
+            let newBoard = assignCell2 num sym board
+            renderBoard board
+            putStrLn ""
+            --Check win and draw
+            let win = checkWin sym newBoard
+            if win == True then do
+                renderBoard newBoard
+                winMessage sym
+                return ()
+            else do
+                if length (possibleMoves4 newBoard) == 0 then do
+                    renderBoard newBoard
+                    putStrLn "DRAW: GAME OVER"
+                    return ()
+                else do
+                    gameLoopMM True X newBoard
+        else do
+            gameLoopMM False O board
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,110 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.Random
+import System.Environment
+import Data.List
+
+data Symbol = X | O
+data Cell   = Occupied Symbol | Blank
+
+
+instance Show Symbol where
+    show X = "X"
+    show O = "O"
+
+instance Show Cell where
+    show (Occupied X)  = "X"
+    show (Occupied O)  = "O"
+    show Blank         = "_"
+
+
+
+instance Eq Cell where
+    Occupied X == Occupied X = True
+    Occupied O == Occupied O = True
+    Blank == Blank           = True
+    _ == _                   = False
+
+
+
+renderRow :: [Cell] -> String
+renderRow row = intercalate " " $ fmap show row
+
+renderBoard :: [Cell] -> IO ()
+renderBoard board = do
+  putStrLn $ "# " ++ renderRow firstRow
+  putStrLn $ "# " ++ renderRow secondRow
+  putStrLn $ "# " ++ renderRow thirdRow
+  where firstRow  = take 3 board
+        secondRow = drop 3 . take 6 $ board
+        thirdRow  = drop 6 board
+
+getBoardIndex :: String -> Maybe Int
+getBoardIndex "1" = Just 0
+getBoardIndex "2" = Just 1
+getBoardIndex "3" = Just 2
+getBoardIndex "4" = Just 3
+getBoardIndex "5" = Just 4
+getBoardIndex "6" = Just 5
+getBoardIndex "7" = Just 6
+getBoardIndex "8" = Just 7
+getBoardIndex "9" = Just 8
+getBoardIndex _    = Nothing
+
+getBoardIndex2 :: String -> Int
+getBoardIndex2 "1" = 0
+getBoardIndex2 "2" = 1
+getBoardIndex2 "3" = 2
+getBoardIndex2 "4" = 3
+getBoardIndex2 "5" = 4
+getBoardIndex2 "6" = 5
+getBoardIndex2 "7" = 6
+getBoardIndex2 "8" = 7
+getBoardIndex2 "9" = 8
+
+-- BRUKES SAMMED MED POSSIBLEMOVES4
+checkIsFree2 :: [Cell] -> Int -> Bool
+checkIsFree2 board i = if board !! i == Blank then True else False
+
+
+-- BRUKES SAMMEN MED CHECKISFREE2
+possibleMoves4 :: [Cell] -> [Int]
+possibleMoves4 board = filter (\x -> checkIsFree2 board x) [0..8]
+
+assignCell :: String -> Symbol -> [Cell] -> [Cell]
+assignCell location symbol board = do
+    let Just x = getBoardIndex location
+    ((take x board) ++ [Occupied symbol] ++ (drop (x+1) board))
+
+assignCell2 :: Int -> Symbol -> [Cell] -> [Cell]
+assignCell2 location symbol board = ((take location board) ++ [Occupied symbol] ++ (drop (location+1) board))
+
+checkRot :: [String] -> Bool
+checkRot xs
+    | length xs == 1 = False
+    | otherwise      = True
+
+checkRotRight :: String -> Bool
+checkRotRight xs
+    | xs == "right" = True
+    | otherwise     = False
+ 
+
+checkWin :: Symbol -> [Cell] -> Bool
+checkWin move board
+    | board !! 0 == (Occupied move) && board !! 1 == (Occupied move) && board !! 2 == (Occupied move) = True
+    | board !! 3 == (Occupied move) && board !! 4 == (Occupied move) && board !! 5 == (Occupied move) = True
+    | board !! 6 == (Occupied move) && board !! 7 == (Occupied move) && board !! 8 == (Occupied move) = True
+    | board !! 0 == (Occupied move) && board !! 3 == (Occupied move) && board !! 6 == (Occupied move) = True
+    | board !! 1 == (Occupied move) && board !! 4 == (Occupied move) && board !! 7 == (Occupied move) = True
+    | board !! 2 == (Occupied move) && board !! 5 == (Occupied move) && board !! 8 == (Occupied move) = True
+    | board !! 0 == (Occupied move) && board !! 4 == (Occupied move) && board !! 8 == (Occupied move) = True
+    | board !! 2 == (Occupied move) && board !! 4 == (Occupied move) && board !! 6 == (Occupied move) = True
+    | otherwise = False
+
+
+rotSwapRight :: [Cell] -> [Cell]
+rotSwapRight xs = case xs of a:b:c:d:e:f:g:h:i:xs -> g:d:c:h:e:b:i:f:a:xs
+
+rotSwapLeft :: [Cell] -> [Cell]
+rotSwapLeft xs = case xs of a:b:c:d:e:f:g:h:i:xs -> a:f:i:b:e:h:c:d:g:xs
